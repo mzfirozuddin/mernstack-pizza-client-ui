eac8b0cf40f70b8093919bdd8f7e8c7784c78030
Mirza Firoz Uddin <mz.firozuddin@gmail.com>
2025-07-08 00:05:01 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
       <main className="">Welcome to pizza shop</main>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
